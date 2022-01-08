09fc3ac59d812500d09b58b6a5f95fcb102d23eb
taniryla <ryland0000@gmail.com>
2022-01-08 02:35:38 -0700

    app js and stuff js file

---
+++ app.js
@@ -0,0 +1,10 @@
+const EventEmitter = require('events');
+
+const myEmitter = new EventEmitter();
+
+// when the event handler function is triggered we get the console log
+myEmitter.on('someEvent', number => {
+    console.log(`started ${number}`);
+});
+
+myEmitter.emit('someEvent', 23);
