e85c14f23334d2bc52681886929a5fa01c087826
taniryla <ryland0000@gmail.com>
2022-01-08 04:00:43 -0700

    node exercises in node repl

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const EventEmitter = require('events');
 
-const myEmitter = new EventEmitter();
 
-// when the event handler function is triggered we get the console log
-myEmitter.on('someEvent', number => {
-    console.log(`started ${number}`);
-});
 
-myEmitter.emit('someEvent', 23);
+
+// const myEmitter = new EventEmitter();
+
+// // when the event handler function is triggered we get the console log
+// myEmitter.on('someEvent', number => {
+//     console.log(`started ${number}`);
+// });
+
+// myEmitter.emit('someEvent', 23);
